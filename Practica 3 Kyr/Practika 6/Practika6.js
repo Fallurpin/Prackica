07bfc8ce3ca379ca2978a418b9ee03b4977f5f10
Fallurpin <67725467+Fallurpin@users.noreply.github.com>
2024-12-04 22:18:33 +0300

    2

--- Practica 3 Kyr/Practika 6/Practika6.js
+++ Practica 3 Kyr/Practika 6/Practika6.js
@@ -3,6 +3,23 @@ a = 10;
 alert("a")
 alert ("Hello World")
 
+// Калькулятор
 
+let res;
+
+const operator = prompt('Введите оператор (\+, \-, \* или /):');
+
+const number1 = parseFloat(prompt('Введите первое число: '));
+const number2 = parseFloat(prompt('Введите второе число: '));
+
+switch(operator) {
+  case '+': res = number1 + number2; alert(`${number1} + ${number2} = ${res}`); break;
+  case '-': res = number1 - number2; alert(`${number1} - ${number2} = ${res}`); break;
+  case '*': res = number1 * number2; alert(`${number1} * ${number2} = ${res}`); break;
+  case '/': res = number1 / number2; alert(`${number1} / ${number2} = ${res}`); break;
+  default: console.log('Неверный оператор'); break;
+}
+
+// Факториал
 
 // Смирнов Дмитрий Исп 309
