e1fa3cce040dd6efbf87c94979afcde5cd125de0
Fallurpin <67725467+Fallurpin@users.noreply.github.com>
2024-12-04 23:14:36 +0300

    4

--- Practica 3 Kyr/Practika 6/Practika6.js
+++ Practica 3 Kyr/Practika 6/Practika6.js
@@ -21,6 +21,12 @@ switch(operator) {
   default: console.log('Неверный оператор'); break;
 }
 
+// Возраст
+var name = prompt('Введите имя');
+var ago = prompt('Ввидите год рождения');
+ago = 2024 - ago;
+alert(name+':'+ago);
+
 // Факториал
 var N = prompt('Введите факториал')
 var M = 1;
@@ -35,4 +41,51 @@ if (M == N){
 }
 
 // Загадка
+var otladke=0;
+otvet = prompt("Ты играешь в Дота 2?")
+if (otvet == 'Да')
+{
+    alert("Верно");
+    otladke = otladke + 1;
+} else{
+    alert("Не верно");
+}
+otvet1 = prompt('Ты гей?')
+if (otvet1 == да)
+{
+alert('Я знаю)');
+otladke = otladke + 1;
+} else{
+alert('Чувствую ложь');
+}
+if(otladke == 2 )
+{
+alert('конец');
+} else{
+alert('Не конец');
+}
+// Угадай число
+let InputNum = prompt("Введите результат умножения вашего числа на 2 и прибавления 7: ");
+
+let UserNumber = parseInt(InputNum);
+
+let origNumber = (UserNumber - 7) / 2;
+
+alert("Вы задумали число: " + origNumber);
+
+// Палиндром
+
+function Palindrome () {
+    return text === text.split('').reverse().join('');
+  }
+  
+  let name = prompt("Введите вашу фамилию:");
+  
+  if (Palindrome(name)) {
+    console.log("Ваша фамилия — палиндром!");
+  } else {
+    console.log("Ваша фамилия — не палиндром.");
+  }
+  
+  
 // Смирнов Дмитрий Исп 309
